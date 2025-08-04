e09a8607d7c9588394415dd4e59cb10227eb96f4
Balram Rajbanshi <balramrajbanshi4769@gmail.com>
2025-08-04 18:43:35 +0545

    cors admin

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ dbConnect()
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cors({
-    origin:["https://book-store-system-virid.vercel.app","http://localhost:5174"],
+    origin:["https://book-store-system-virid.vercel.app","https://book-store-admin-ee1v.vercel.app"],
     methods:["GET","POST","PUT","DELETE","PATCH"],
     credentials:true
 }))
